e1bc0d255f4452ff4ae92b2e4b5cf5e12a02e47c
Jack Wotherspoon <jackwoth@google.com>
2025-12-27 16:47:27 -0500

    chore: update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -48,7 +48,7 @@ export const buildTargets = async (version: string) => {
 // Allow running directly
 if (import.meta.main) {
   const version = process.argv[2] || pkg.version;
-  console.log(`\n🎁 Building oc-wrapped v${version}\n`);
+  console.log(`\n🎁 Building gemini-wrapped v${version}\n`);
   await buildTargets(version);
   console.log("\n✅ Build complete\n");
 }
